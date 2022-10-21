c7121930a1492e1f7db6475798ee0f58d5f02804
Paul-scpark <75621410+Paul-scpark@users.noreply.github.com>
2022-10-22 00:08:13 +0900

    Modified week5 - monthly project (django, heroku)

--- 5주차/monthly_prj/monthly_prj/settings.py
+++ 5주차/monthly_prj/monthly_prj/settings.py
@@ -10,8 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import os
-import django_on_heroku
+import os, dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%6^&e^ynpuq*&6x3@lh&&&=csz(i^o1t64giq%tj$y!*=fl3lf'
+ORI_SECRET_KEY = 'django-insecure-%6^&e^ynpuq*&6x3@lh&&&=csz(i^o1t64giq%tj$y!*=fl3lf'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', ORI_SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,11 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, "app", "static/")
+STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_on_heroku.settings(locals())
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
